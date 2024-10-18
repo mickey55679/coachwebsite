1145513da674af92e67490bda2186c985520110f
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-18 18:10:31 -0500

    added javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+    //get sidebar element 
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSideBar(){
+   const sidebar = document.querySelector(".sidebar");
+   sidebar.style.display = "none";
+}
