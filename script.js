82006f85517a4f15f3037b36a90e5160566f4893
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-18 18:41:22 -0500

    added pointer events so click still works

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showSideBar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
-function hideSideBar(){
-   const sidebar = document.querySelector(".sidebar");
-   sidebar.style.display = "none";
+function hideSideBar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
