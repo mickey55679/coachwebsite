1c4b212af504ea44b0ee3aae705a01509668a765
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-23 15:53:22 -0500

    wip

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function hideSideBar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Function to change slides
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+// Function to display the current slide
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+}
+
+// Optional: Auto slideshow
+// setInterval(() => {
+//   changeSlide(1);
+// }, 1000); 
